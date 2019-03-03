a4e54963080d60e71cb158cce4b16f4114215f5c
Asbjørn Enge <asbjorn@hanafjedle.net>
2019-03-03 12:51:19 +0100

    Attempt at computing baking and endorsements misses and steals

--- index.js
+++ index.js
@@ -34,10 +34,20 @@ var blocks_stolen = prom.newCounter({
   name: 'blocks_stolen',
   help: '# Blocks stolen'
 })
-var operation_endorsement_success = prom.newCounter({
+var operation_endorsements = prom.newCounter({
   namespace: 'tezos_baker',
-  name: 'operation_endorsement_success',
+  name: 'operation_endorsements',
   help: '# Endorsements made'
+})
+var operation_endorsements_missed = prom.newCounter({
+  namespace: 'tezos_baker',
+  name: 'operation_endorsements_missed',
+  help: '# Endorsements missed'
+})
+var operation_endorsements_stolen = prom.newCounter({
+  namespace: 'tezos_baker',
+  name: 'operation_endorsements_stolen',
+  help: '# Endorsements stolen'
 })
 var balance_total = prom.newGauge({
   namespace: 'tezos_baker',
@@ -83,12 +93,27 @@ var query = async () => {
   let head_block = head.header.level
   if (head_block === last_block_processed)
     return
+  console.log(`Processing block ${head_block}`)
   last_block_processed = head_block
 
-  // Blocks
+  // Bakes 
 
   let baker = head.metadata.baker
   if (baker === args.baker) blocks_baked.increment(labels) 
+
+  let bakingRights = await fetch(`${baseUri}/chains/main/blocks/${head_block}/helpers/baking_rights?delegate=${args.baker}&level=${head_block}&all`)
+    .then(res => res.json())
+    .catch(err => console.error(err.message))
+
+  let shouldHaveBaked = 0
+  bakingRights.forEach(br => {
+    if (br.priority === 0)
+      shouldHaveBaked++
+  })
+  if (shouldHaveBaked > 0 && baker !== args.baker)
+    blocks_missed.increment(labels)
+  if (shouldHaveBaked === 0 && baker === args.baker)
+    blocks_stolen.increment(labels)
 
   // Endorsements
 
@@ -101,7 +126,21 @@ var query = async () => {
       })
     })
   })
-  if (endorsements) operation_endorsement_success.increment(labels, endorsements) 
+  if (endorsements) operation_endorsements.increment(labels, endorsements) 
+
+  let endorsingRights = await fetch(`${baseUri}/chains/main/blocks/${head_block}/helpers/endorsing_rights?delegate=${args.baker}&level=${head_block}`)
+    .then(res => res.json())
+    .catch(err => console.error(err.message))
+
+  let shouldHaveEndorsed = 0
+  endorsingRights.forEach(er => {
+    if (er.slots.indexOf(0) >= 0)
+      shouldHaveEndorsed++
+  })
+  if (shouldHaveEndorsed > endorsements)
+    operation_endorsements_missed.increment(labels)
+  if (shouldHaveEndorsed < endorsements) 
+    operation_endorsements_stolen.increment(labels)
 
   // Balance
 
